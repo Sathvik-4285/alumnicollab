395ea6458dc12bc49e58d525351ac9daf5637bd0
Sathvik-4285 <v.sathvik2004@gmail.com>
2026-05-16 12:39:56 +0530

    update backend url

--- OneDrive/Desktop/projects/aluminicollab/AlumniCollab/client/src/pages/Login.js
+++ OneDrive/Desktop/projects/aluminicollab/AlumniCollab/client/src/pages/Login.js
@@ -24,7 +24,7 @@ function Login() {
 
     const response =
       await axios.post(
-        "http://localhost:5000/api/auth/login",
+        "https://alumnicollab.onrender.com/",
         {
           email: formData.email,
           password: formData.password,
